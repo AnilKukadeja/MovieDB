2f71bcec8bb1711c8b46c6a3cfdc9b6a12679e4f
Anil Kukadeja <anil.kukadeja01@gmail.com>
2018-04-30 20:25:58 +0530

    Iniital commit - project setup

---
+++ MovieDB/MovieDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Parth Adroja on 30/04/18.
+//  Copyright © 2018 Anil Kukadeja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
