1c408219d8c3cde531b27b2f53f9dd1bb67fcea3
Anil Kukadeja <anil.kukadeja01@gmail.com>
2018-05-01 21:12:33 +0530

    added loading indicator

--- MovieDB/MovieDB/ViewControllers/MoviesListViewController.swift
+++ MovieDB/MovieDB/ViewControllers/MoviesListViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import Alamofire
 import ObjectMapper
 import Kingfisher
+import SVProgressHUD
 
 class MoviesListViewController: UIViewController {
     
@@ -18,7 +19,7 @@ class MoviesListViewController: UIViewController {
     var currentPage = 1
     var totalPage = 1
     
-    var moviesList: [MovieList]?
+    var moviesList = [MovieList]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,7 +37,7 @@ class MoviesListViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         UIApplication.shared.statusBarStyle = .default
     }
-
+    
 }
 
 // MARK: Custom Methods
@@ -54,34 +55,39 @@ extension MoviesListViewController{
     
     func getMovieList(currentPage : Int){
         
-            // Add URL parameters
-            let urlParams = [
-                WebServiceAPIConstants.apiKey: "201f3add5604b108ffe6d1d53dd54a87",
-                WebServiceAPIConstants.page: currentPage
-                ] as [String : Any]
-            
-            // Fetch Request
-            Alamofire.request(WebServiceAPIConstants.nowPlayingMovies, method: .get, parameters: urlParams)
-                .validate(statusCode: 200..<300)
-                .responseJSON { response in
-                    if (response.result.error == nil) {
-                        if let resultDictonary = response.result.value as? [String:Any] {
-                            if let mappedModel = Mapper<ResponseModel>().map(JSON: resultDictonary){
-                                if let totalPage = mappedModel.total_pages {
-                                    self.totalPage = totalPage
-                                }
-                                if let movies = mappedModel.results, movies.count > 0 {
-                                    self.moviesList?.append(contentsOf: movies)
-                                    DBManager.shared.insertMoviesToDB(movies: movies)
-                                }
-                                
-                                self.reloadTableView()
+        // Add URL parameters
+        let urlParams = [
+            WebServiceAPIConstants.apiKey: "201f3add5604b108ffe6d1d53dd54a87",
+            WebServiceAPIConstants.page: currentPage
+            ] as [String : Any]
+        
+        
+        SVProgressHUD.setDefaultMaskType(.black)
+        SVProgressHUD.show()
+
+        // Fetch Request
+        Alamofire.request(WebServiceAPIConstants.nowPlayingMovies, method: .get, parameters: urlParams)
+            .validate(statusCode: 200..<300)
+            .responseJSON { response in
+                SVProgressHUD.dismiss()
+                if (response.result.error == nil) {
+                    if let resultDictonary = response.result.value as? [String:Any] {
+                        if let mappedModel = Mapper<ResponseModel>().map(JSON: resultDictonary){
+                            if let totalPage = mappedModel.total_pages {
+                                self.totalPage = totalPage
                             }
+                            if let movies = mappedModel.results, movies.count > 0 {
+                                self.moviesList.append(contentsOf: movies)
+                                DBManager.shared.insertMoviesToDB(movies: movies)
+                            }
+                            
+                            self.reloadTableView()
                         }
                     }
-                    else {
-                    }
-            }
+                }
+                else {
+                }
+        }
     }
     
 }
@@ -95,7 +101,7 @@ extension MoviesListViewController : UITableViewDataSource,UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return moviesList?.count ?? 0
+        return moviesList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -104,14 +110,13 @@ extension MoviesListViewController : UITableViewDataSource,UITableViewDelegate {
             return UITableViewCell()
         }
         
-        if let movieList = moviesList {
-            cell.labelMovieName.text = movieList[indexPath.row].title ?? "Default Title"
-            cell.labelReleaseDate.text = movieList[indexPath.row].releaseDate ?? "Default Date"
-            
-            if let imageUrl = movieList[indexPath.row].posterPath{
-                    cell.imgMovieThumbnail.kf.setImage(with: URL(string: WebServiceAPIConstants.imageURL + imageUrl))
-            }
-            
+        cell.labelMovieName.text = moviesList[indexPath.row].title ?? "Default Title"
+        cell.labelReleaseDate.text = moviesList[indexPath.row].releaseDate ?? "Default Date"
+        
+        if let imageUrl = moviesList[indexPath.row].posterPath{
+            cell.imgMovieThumbnail.kf.setImage(with: URL(string: WebServiceAPIConstants.imageURL + imageUrl))
+        }else{
+            cell.imgMovieThumbnail.image = nil
         }
         
         return cell
@@ -124,22 +129,21 @@ extension MoviesListViewController : UITableViewDataSource,UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         guard let movieDetailViewControllerObj = self.storyboard?.instantiateViewController(withIdentifier: "MovieDetailViewController") as? MovieDetailViewController else { return }
-        movieDetailViewControllerObj.movieDetails = moviesList?[indexPath.row]
+        movieDetailViewControllerObj.movieDetails = moviesList[indexPath.row]
         self.navigationController?.pushViewController(movieDetailViewControllerObj, animated: true)
     }
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         
-        if let moviesList = moviesList{
+        
+        if(indexPath.row == (moviesList.count) - 1){
             
-            if(indexPath.row == (moviesList.count) - 1){
-                
-                if(currentPage <= totalPage){
-                    getMovieList(currentPage: currentPage)
-                    currentPage += 1
-                }
+            if(currentPage <= totalPage){
+                getMovieList(currentPage: currentPage)
+                currentPage += 1
             }
         }
+        
     }
     
 }
